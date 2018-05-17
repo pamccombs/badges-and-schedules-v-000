2ccbb1d85a571f20727ea4f8f46be125929f4a43
Patrick McCombs <pa.mccombs1@gmail.com>
2018-05-17 17:26:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,7 @@ end
 
 def assign_rooms(name)
   room = [1..7]
-  name.collect{|list| "Hello, #{list}! You'll be assigned to room #{room.each_with_index}!"}
+  name.collect{|list| "Hello, #{list}!"}
+  room.each_with index{"You'll be assigned to room #{room}!"
   
 end
